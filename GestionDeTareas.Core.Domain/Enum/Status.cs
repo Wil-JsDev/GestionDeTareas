d50718c9776b59c7d56ef3bff878995b27b6b282
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-19 13:36:24 -0400

    feat: add models

---
+++ GestionDeTareas.Core.Domain/Enum/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeTareas.Core.Domain.Enum
+{
+    public enum Status
+    {
+        Completed = 0,
+        Pending = 1 
+    }
+}
---
+++ GestionDeTareas.Core.Domain/Models/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using GestionDeTareas.Core.Domain.Enum;
+
+namespace GestionDeTareas.Core.Domain.Models
+{
+    public sealed class TaskItem
+    {
+        public Guid Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateOnly DuaDate { get; set; }
+
+        public Status Status { get; set; }
+
+        public int AdditionalData { get; set; }
+    }
+}
