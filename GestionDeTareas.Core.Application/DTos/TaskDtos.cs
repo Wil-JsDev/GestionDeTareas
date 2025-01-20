c1330753f1d06f48936d9833893a48bde26f7191
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-19 22:05:33 -0400

    feat: add dtos

---
+++ GestionDeTareas.Core.Application/DTos/TaskDtos.cs
@@ -0,0 +1,18 @@
+﻿using GestionDeTareas.Core.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeTareas.Core.Application.DTos
+{
+    public record TaskDtos
+    (
+        Guid Id,
+        string Description,
+        DateOnly DuaDate,
+        Status Status,
+        int AdditionalData
+   );
+}
---
+++ GestionDeTareas.Core.Application/DTos/UpdateTaskDtos.cs
@@ -0,0 +1,15 @@
+﻿using GestionDeTareas.Core.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeTareas.Core.Application.DTos
+{
+    public record UpdateTaskDtos
+    (
+         string Description,
+         Status Status
+    );
+}
