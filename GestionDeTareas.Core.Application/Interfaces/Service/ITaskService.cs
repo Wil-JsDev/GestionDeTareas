a47e54b383044f9ac3f7d005d3e040f35aa44d32
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-19 22:23:06 -0400

    feat: implement task service, repository, and generic repository

---
+++ GestionDeTareas.Core.Application/Interfaces/Service/ITaskService.cs
@@ -0,0 +1,25 @@
+﻿using GestionDeTareas.Core.Application.DTos;
+using GestionDeTareas.Core.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeTareas.Core.Application.Interfaces.Service
+{
+    public interface ITaskService
+    {
+        Task<TaskDtos> CreateAsync(CreateTaskDto task, CancellationToken cancellationToken);
+
+        Task<TaskDtos> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<TaskDtos>> GetlAllAsync(CancellationToken cancellationToken);
+
+        Task<IEnumerable<TaskDtos>> FilterByStatus(Status status, CancellationToken cancellationToken);
+
+        Task<TaskDtos> DeleteAsync(Guid id, CancellationToken cancellationToken);
+
+        Task<TaskDtos> UpdateAsync(Guid id, UpdateTaskDtos updateTaskDtos, CancellationToken cancellationToken);
+    }
+}
