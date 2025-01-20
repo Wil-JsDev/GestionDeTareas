2c079bf0f1bfe606203e1d90f7f735652dd23d6f
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-19 22:43:14 -0400

    feat: add task service and mapper dependency injection

---
+++ GestionDeTareas.Core.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using GestionDeTareas.Core.Application.Interfaces.Service;
+using GestionDeTareas.Core.Application.Mapper;
+using GestionDeTareas.Core.Application.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestionDeTareas.Core.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection service)
+        {
+            service.AddAutoMapper(typeof(MappingProfile));
+
+            #region Services
+            service.AddScoped<ITaskService, TaskService>();
+            #endregion
+
+            return service;
+        }
+    }
+}
