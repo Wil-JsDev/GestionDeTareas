bab289b4e0da219e0785c74069cb9755a06949c9
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-30 18:28:08 -0400

    feat: add result pattern in API

--- GestionDeTareas.Presentation.Api/Controllers/TaskController.cs
+++ GestionDeTareas.Presentation.Api/Controllers/TaskController.cs
@@ -1,7 +1,6 @@
 ﻿using GestionDeTareas.Core.Application.DTos;
 using GestionDeTareas.Core.Application.Interfaces.Service;
 using GestionDeTareas.Core.Domain.Enum;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GestionDeTareas.Presentation.Api.Controllers
@@ -20,41 +19,77 @@ namespace GestionDeTareas.Presentation.Api.Controllers
         public async Task<IActionResult> CreateAsync([FromBody] CreateTaskDto createTaskDtos, CancellationToken cancellationToken)
         {
             var taskDto = await _taskService.CreateAsync(createTaskDtos, cancellationToken);
+            if (!taskDto.IsSuccess)
+            {
+                return BadRequest(taskDto.Error);
+            }
+            
+            return Ok(taskDto);
 
-            return taskDto == null ? BadRequest() : Ok(taskDto);
         }
 
         [HttpGet]
-        public async Task<IEnumerable<TaskDtos>> GetAllAsync(CancellationToken cancelationToken) =>
-           await _taskService.GetlAllAsync(cancelationToken);
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancelationToken)
+        {
+            var taskItems = await _taskService.GetlAllAsync(cancelationToken);
+            if (!taskItems.IsSuccess)
+            {
+                return BadRequest(taskItems.Error);
+            }
+
+            return Ok(taskItems);
+        }
 
         [HttpGet("{status}")]
         public async Task<IActionResult> FilterByStatusAsync([FromRoute] Status status, CancellationToken cancellationToken)
         {
             var filter = await _taskService.FilterByStatus(status, cancellationToken);
 
-            return filter == null ? BadRequest() : Ok(filter);
+            if (!filter.IsSuccess)
+            {
+                return BadRequest(filter.Error);
+            }
+            
+            return Ok(filter);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetByIdAsync([FromRoute] Guid id, CancellationToken cancellationToken)
         {
             var task = await _taskService.GetByIdAsync(id, cancellationToken);
-            return task == null ? NotFound($" {id} not found") : Ok(task);
+            if (!task.IsSuccess)
+            {
+                return NotFound(task.Error);
+            }
+
+            return Ok(task);
+
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAsync([FromRoute] Guid id, CancellationToken cancellationToken)
         {
             var task = await _taskService.DeleteAsync(id, cancellationToken);
-            return task == null ? NotFound($"{id} not found") : NoContent();
+            if (!task.IsSuccess)
+            {
+                return NotFound(task.Error);
+            }
+
+            return Ok(task);
+
         }
 
         [HttpPut]
         public async Task<IActionResult> UpdateAsync([FromQuery] Guid id, [FromBody] UpdateTaskDtos updateTask, CancellationToken cancellationToken)
         {
             var task = await _taskService.UpdateAsync(id, updateTask, cancellationToken);
-            return task == null ? NotFound() : Ok(task);
+            if(!task.IsSuccess)
+            {
+                return NotFound(task.Error);
+            }
+
+            return Ok(task);
+
         }
 
     }
