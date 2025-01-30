e40189a5ac2e1dd576f62f33d613f2b2c1aa5614
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-30 18:28:52 -0400

    feat: add result pattern in utils

---
+++ GestionDeTareas.Core.Domain/Utils/Error.cs
@@ -0,0 +1,35 @@
+﻿using GestionDeTareas.Core.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeTareas.Core.Domain.Utils
+{
+    public class Error
+    {
+        private Error(
+            string code,
+            string description,
+            ErrorType errorType
+            )
+        {
+            Code = code;
+            Description = description;
+            ErrorType = errorType;
+        }
+
+        public string Code { get; }
+
+        public string Description { get; }
+
+        public ErrorType ErrorType { get; }
+
+        public static Error Failure(string code, string description) =>
+            new(code, description, ErrorType.Failure);
+
+        public static Error NotFound(string code, string description) =>
+            new(code, description, ErrorType.NotFound);
+    }
+}
---
+++ GestionDeTareas.Core.Domain/Utils/Result.cs
@@ -0,0 +1,54 @@
+﻿namespace GestionDeTareas.Core.Domain.Utils
+{
+    public class Result
+    {
+        public bool IsSuccess { get; set; }
+
+        public Error? Error { get; set; }
+
+        protected Result()
+        {
+            IsSuccess = true;
+            Error = default;
+        }
+
+        protected Result(Error error)
+        {
+            IsSuccess = false;
+            Error = error;
+        }
+
+        public static Result Success() =>
+           new();
+
+        public static Result Failure(Error error) =>
+            new(error);
+    }
+
+    public sealed class ResultT<TValue> : Result
+    {
+        private readonly TValue _value;
+
+        private ResultT(TValue value) : base()
+        {
+            _value = value;
+        }
+
+        public static implicit operator ResultT<TValue>(Error error) =>
+            new(error);
+
+        public static implicit operator ResultT<TValue>(TValue value) =>
+            new(value);
+
+        private ResultT(Error error) : base(error)
+        {
+            _value = default;
+        }
+
+        public static ResultT<TValue> Success(TValue value) =>
+            new(value);
+
+        public static ResultT<TValue> Failure(Error error) =>
+            new(error);
+    }
+}
