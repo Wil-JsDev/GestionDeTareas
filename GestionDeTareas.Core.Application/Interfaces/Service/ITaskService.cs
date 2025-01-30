8bcb42340dc7e3eba98db6f3f568e70cdead948c
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-30 18:27:54 -0400

    feat: add result pattern

--- GestionDeTareas.Core.Application/Interfaces/Service/ITaskService.cs
+++ GestionDeTareas.Core.Application/Interfaces/Service/ITaskService.cs
@@ -1,25 +1,21 @@
 ﻿using GestionDeTareas.Core.Application.DTos;
 using GestionDeTareas.Core.Domain.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GestionDeTareas.Core.Domain.Utils;
 
 namespace GestionDeTareas.Core.Application.Interfaces.Service
 {
     public interface ITaskService
     {
-        Task<TaskDtos> CreateAsync(CreateTaskDto task, CancellationToken cancellationToken);
+        Task<ResultT<TaskDtos>> CreateAsync(CreateTaskDto task, CancellationToken cancellationToken);
 
-        Task<TaskDtos> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task <ResultT<TaskDtos>> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 
-        Task<IEnumerable<TaskDtos>> GetlAllAsync(CancellationToken cancellationToken);
+        Task<ResultT<IEnumerable<TaskDtos>>> GetlAllAsync(CancellationToken cancellationToken);
 
-        Task<IEnumerable<TaskDtos>> FilterByStatus(Status status, CancellationToken cancellationToken);
+        Task<ResultT<IEnumerable<TaskDtos>>> FilterByStatus(Status status, CancellationToken cancellationToken);
 
-        Task<TaskDtos> DeleteAsync(Guid id, CancellationToken cancellationToken);
+        Task<ResultT<TaskDtos>> DeleteAsync(Guid id, CancellationToken cancellationToken);
 
-        Task<TaskDtos> UpdateAsync(Guid id, UpdateTaskDtos updateTaskDtos, CancellationToken cancellationToken);
+        Task<ResultT<TaskDtos>> UpdateAsync(Guid id, UpdateTaskDtos updateTaskDtos, CancellationToken cancellationToken);
     }
 }
