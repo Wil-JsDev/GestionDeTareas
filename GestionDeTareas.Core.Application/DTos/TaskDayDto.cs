c4bfd1d01bdbd182599335eaebe5c6ac56c5b1ac
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-30 22:20:18 -0400

    feat: add task day dto

---
+++ GestionDeTareas.Core.Application/DTos/TaskDayDto.cs
@@ -0,0 +1,14 @@
+﻿using GestionDeTareas.Core.Domain.Enum;
+
+namespace GestionDeTareas.Core.Application.DTos
+{
+    public sealed record TaskDayDto
+    (
+        Guid TaskId,
+        string Description,
+        DateOnly DuaDate,
+        Status Status,
+        int DayLeft,
+        int AdditionalData
+    );
+}
