3c18e7b128c8fc34ac494470542c68dacfc6fcb4
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-30 22:16:39 -0400

    feat: add validate

--- GestionDeTareas.Core.Application/Interfaces/Repository/ITaskRepository.cs
+++ GestionDeTareas.Core.Application/Interfaces/Repository/ITaskRepository.cs
@@ -1,15 +1,12 @@
-﻿using GestionDeTareas.Core.Domain.Enum;
-using GestionDeTareas.Core.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GestionDeTareas.Core.Domain.Models;
 
 namespace GestionDeTareas.Core.Application.Interfaces.Repository
 {
     public interface ITaskRepository : IGenericRepository<TaskItem>
     {
         Task<IEnumerable<TaskItem>> GetFilterAsync( Func<TaskItem, bool> predicate, CancellationToken cancellationToken);
+
+        Task<bool> ValidateAsync(Func<TaskItem, bool> validate);
+
     }
 }
