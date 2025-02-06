7f2afbb5b5dce2276dbb71c6139022998f65f412
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-06 11:35:49 -0400

    feat: add high priority factory

---
+++ GestionDeTareas.Core.Application/Factories/HighPriority/ConcreteHighPriorityFactory .cs
@@ -0,0 +1,13 @@
+﻿using GestionDeTareas.Core.Application.Interfaces.Factories;
+
+namespace GestionDeTareas.Core.Application.Factories.HighPriority
+{
+    //Concrete Factory
+    public class ConcreteHighPriorityFactory : HighPriorityFactory
+    {
+        public override IHighPriority CreateHighPriority()
+        {
+            return new HighPriorityTask();
+        }
+    }
+}
