76e9411fde215c5e65e2e54d053cb420b03af65a
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-06 11:36:06 -0400

    feat: add three days factory

---
+++ GestionDeTareas.Core.Application/Factories/ThreeDayTask/ConcreteThreeDayTaskFactory.cs
@@ -0,0 +1,13 @@
+﻿
+using GestionDeTareas.Core.Application.Interfaces.Factories;
+
+namespace GestionDeTareas.Core.Application.Factories.ThreeDayTask
+{
+    public class ConcreteThreeDayTaskFactory : ThreeDayTaskFactory
+    {
+        public override IThreeDay CreateTaskThreeDays()
+        {
+            return new ThreeDayTask();
+        }
+    }
+}
---
+++ GestionDeTareas.Core.Application/Factories/ThreeDayTask/ThreeDayTask.cs
@@ -0,0 +1,16 @@
+﻿using GestionDeTareas.Core.Application.Interfaces.Factories;
+
+namespace GestionDeTareas.Core.Application.Factories.ThreeDayTask
+{
+    public class ThreeDayTask : IThreeDay
+    {
+        private DateOnly _date;
+
+        public void SetDays(DateOnly date)
+        {
+            _date = date;
+        }
+
+        public DateOnly GetDays() => _date;
+    }
+}
