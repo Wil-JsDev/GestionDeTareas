c0b50d4ae3d326361cf88d42d822e35270903320
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-01 22:47:27 -0400

    feat: add account dtos

---
+++ GestionDeTareas.Core.Application/DTos/Account/Authenticate/AuthenticateResponse.cs
@@ -0,0 +1,18 @@
+﻿
+namespace GestionDeTareas.Core.Application.DTos.Account.Authenticate
+{
+    public sealed class AuthenticateResponse
+    {
+        public string? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public List<string> Roles { get; set; }
+        public bool? IsVerified { get; set; }
+        public int StatusCodes { get; set; }
+        public string? JWTToken { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
